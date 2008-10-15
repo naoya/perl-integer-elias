edcde72a20545fe1c31bcf5db483dbe2336123ea
naoya <naoya@hatena.ne.jp>
2008-10-16 00:38:12 +0900

    remove comment

--- lib/Integer/Elias/Encoder.pm
+++ lib/Integer/Elias/Encoder.pm
@@ -15,9 +15,6 @@ sub alpha_encode {
 
 sub gamma_encode {
     my ($self, $n) = @_;
-
-    ## 例: n == 3 のときは n + 1 = 4 を考える
-    ## 0, 1, 2, 3, 4 の 5 種類 (n + 1) + 1 = n + 2 のエントロピー => lg 5
     my $nbits = ceil( log($n + 2) / log(2) );
     $self->alpha_encode($nbits - 1);
     $self->putbits($nbits - 1, $n + 1);
